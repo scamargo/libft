db8aab91760e75d57b850e2d19a7e21468befcc7
Sergio Camargo <sergio.camargo813@gmail.com>
2017-12-08 16:57:35 -0800

    fix ft_sqrt

--- ft_sqrt.c
+++ ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: scamargo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/27 16:53:40 by scamargo          #+#    #+#             */
-/*   Updated: 2017/12/08 14:01:05 by scamargo         ###   ########.fr       */
+/*   Updated: 2017/12/08 16:55:49 by scamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int		ft_sqrt(int nb)
 	int sqr;
 	int root;
 
-	sqr = 0;
+	sqr = 1;
 	root = 1;
 	while (sqr != nb)
 	{
+		root++;
 		sqr = root * root;
 		if (sqr > nb)
 			return (0);

0463503ad153e148263b78e728ecd87720316311
Sergio Camargo <sergio.camargo813@gmail.com>
2017-12-17 21:48:54 -0800

    normed ft_init_chartable

--- ft_init_chartable.c
+++ ft_init_chartable.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init_chartable.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scamargo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/17 21:46:15 by scamargo          #+#    #+#             */
+/*   Updated: 2017/12/17 21:48:28 by scamargo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "libft.h"
-  
+
 char	**ft_init_chartable(size_t width, size_t height)
 {
-    char	**tab;
+	char	**tab;
 	size_t	i;
- 
-    if(!(tab = (char**)ft_memalloc(sizeof(char*) * height)))
+
+	if (!(tab = (char**)ft_memalloc(sizeof(char*) * height)))
 		return (NULL);
-    if(!(tab[0] = (char*)ft_memalloc(sizeof(char) * width * height)))
+	if (!(tab[0] = (char*)ft_memalloc(sizeof(char) * width * height)))
 		return (NULL);
- 	i = 0;
+	i = 0;
 	while (i < height)
 	{
 		tab[i] = *tab + width * i;
 		i++;
 	}
-    return (tab);
+	return (tab);
 }
